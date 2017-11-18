30787cc89652c12f43c1f9b5fbc7dc633731ed93
dorash44 <dorash44!@gmail.com>
2017-11-18 10:40:12 +0200

    fainding titles

--- DOR.py
+++ DOR.py
@@ -1,6 +1,14 @@
-import random
+import requests
+from bs4 import BeautifulSoup
 
-lis = []
-for i in range(10):
-    lis.append(random.randrange(1,100))
-print(lis)
+def article_titles():# function that print all titles in new your times
+    url = "https://www.nytimes.com/"
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    for link in soup.findAll('h2', {'class': 'story-heading'}):  # takes all the links of titles
+        title = link.string  # save the title
+        print(title)
+
+
+article_titles()
